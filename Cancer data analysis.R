8d2d28c6cd5a11e1dd5440379b973017337efba7
ellaintheclouds <138864301+ellaintheclouds@users.noreply.github.com>
2024-02-02 09:16:12 +0000

    Add files via upload
    changed A and V cell line labels as these were the wrong way around and streamlined processing

--- Cancer data analysis.R
+++ Cancer data analysis.R
@@ -16,40 +16,34 @@ View(ex1_data)
 
 # Proportion of all mutations that the most frequently mutated amino acid 
 # accounts for
-ex1_data$proportion_freq_aa <- NA
-
-for(i in 1:10){
-  ex1_data[i, "proportion_freq_aa"] <- 
-    ex1_data[i, "n_mut_at_freq"]/ex1_data[i, "n_mut"]
-}
+ex1_data$proportion_freq_aa <- ex1_data$n_mut_at_freq/ex1_data$n_mut
 
 # The proportion of truncating mutations, for each gene
-for(i in 1:10){
-  ex1_data[i, "proportion_trunc_mut"] <- 
-    ex1_data[i, "n_truncating_mut"]/ex1_data[i, "n_mut"]
-}
+ex1_data$proportion_trunc_mut <- ex1_data$n_truncating_mut/ex1_data$n_mut
 
 # Plot----------
 # Scatterplot of these two sets of values plotted against each other
-title_label <- "Novel Cancer Mutational Profile: 
-Proportion of Truncations vs Mutational 'Hot-Spots'"
-
 ex1_plot <- 
   ggplot(ex1_data, aes(x = proportion_freq_aa, y = proportion_trunc_mut)) +
   geom_point(aes(colour  = gene_name), size = 2) +
-  xlab("Proportion of Mutations that Most Frequently Mutated Amino Acid Accounts For") + 
+  scale_x_continuous(limits = c(0, 1), expand = expansion(mult = c(0, 0))) + 
+  scale_y_continuous(limits = c(0, 0.25), expand = expansion(mult = c(0, 0))) + 
+  xlab("Proportion of Total Mutations by 
+       Most Frequently Mutated Amino Acid") + 
   ylab("Proportion of Truncating Mutations") +
-  labs(title = title_label,
-       colour = "Gene") + 
-  scale_colour_brewer(palette = "Spectral") + 
-  theme_bw()
-
-# Interactive plot
-ggplotly(ex1_plot)
+  labs(title = "Novel Cancer Mutational Profile: Proportion of 
+       Truncations vs Mutational 'Hot-Spots'", colour = "Gene") + 
+  scale_colour_brewer(palette = "Paired") + 
+  theme_bw() +
+  theme(plot.title = element_text(size=12, face="bold", 
+                                  margin = margin(10, 0, 10, 0)))
 
 # Save
 ggsave(ex1_plot, filename = "Output/Experiment 1 scatterplot.png", 
-       width = 7, height = 6)
+       width = 4.5, height = 4)
+
+# Interactive plot
+ggplotly(ex1_plot)
 
 
 # Experiment 2 -----------------------------------------------------------------
@@ -64,7 +58,7 @@ rownames(ex2v_data) <- ex2v_data[,1]
 ex2v_data <- ex2v_data[,-1]
 
 # Putting A and V data in to a list
-ex2_data <- list(ex2a_data, ex2v_data)
+ex2_data <- list("A" = ex2a_data, "V" = ex2v_data)
 
 # Calculations required to find log survival----------
 for(i in 1:2){
@@ -76,36 +70,38 @@ for(i in 1:2){
   
   # % Survival (efficiency at each dose of radiation/efficiency in untreated)
   ex2_data[[i]]["survival",] <- (ex2_data[[i]]["efficiency",]/ex2_data[[i]]["efficiency", 1])*100
-  
-  # Log % survival
-  ex2_data[[i]]["log_survival",] <- log(ex2_data[[i]]["survival",])
 }
 
-View(ex2_data[[1]])
-View(ex2_data[[2]])
+View(ex2_data[["A"]])
+View(ex2_data[["V"]])
 
 # Plot----------
-ex2_title <- "Survival of B353-V and B353-A Cell Lines Exposed to Ionising Radiation"
-
 plot_df <- data.frame("dose" = c(0, 1, 5, 10, 20),
-                      "a_cells" = as.numeric(ex2_data[[1]]["log_survival",]),
-                      "v_cells" = as.numeric(ex2_data[[2]]["log_survival",]))
+                      "a_cells" = as.numeric(ex2_data[["A"]]["survival",]),
+                      "v_cells" = as.numeric(ex2_data[["V"]]["survival",]))
 
 ex2_plot <- ggplot(plot_df) + 
   geom_line(aes(x = dose, y = a_cells, colour = "a_cells")) + 
   geom_point(aes(x = dose, y = a_cells, colour = "a_cells"), size = 2) +
   geom_line(aes(x = dose, y = v_cells, colour = "v_cells")) + 
   geom_point(aes(x = dose, y = v_cells, colour = "v_cells"), size = 2) + 
-  scale_color_manual(name = "Cell Line", labels = c("B353-V", "B353-A"), 
+  scale_color_manual(name = "Cell Line", labels = c("B353-A", "B353-V"), 
                      values = c("a_cells" = "cornflowerblue", 
-                                 "v_cells" = "deeppink4")) + 
-  xlab("Dose of Ionising Mutation (Grays)") + ylab("log(% Survival of Cells)") + 
-  labs(title = ex2_title) +
-  theme_bw()
-
-# Interactive Plot
-ggplotly(ex2_plot)
+                                 "v_cells" = "darkorchid4")) + 
+  xlab("Dose of Ionising Mutation (Gray)") + ylab("Surviving Fraction (%)") + 
+  labs(title = "Survival of B353-V and B353-A Cell Lines
+       Exposed to Ionising Radiation") +
+  scale_x_continuous(expand = expansion(mult = c(0, 0.005))) + 
+  scale_y_continuous(trans="log10", limits = c(NA, 100), 
+                     expand = expansion(mult = c(0, 0))) + 
+  annotation_logticks(sides = "l") +
+  theme_bw() + 
+  theme(plot.title = element_text(size=12, face="bold", 
+                                  margin = margin(10, 0, 10, 0)))
 
 # Save
 ggsave(ex2_plot, filename = "Output/Experiment 2 survival rplot.png", 
-       width = 7, height = 6)
+       width = 5, height = 4)
+
+# Interactive Plot
+ggplotly(ex2_plot)
